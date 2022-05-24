ae0ff25764831c07a5ff13022d9a8a8c52b219a8
Anais <avalverdea54@gmail.com>
2022-05-24 12:37:10 -0500

    boton continuar mal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import validator from './validator.js';
 
 console.log(validator);
+function ir(){
+    let vista1=document.getElementById("ventana2");
+    vista1.style.display="block";
+    let vista2=document.getElementById("ventana1");
+    vista2.style.display="none";
+  }
