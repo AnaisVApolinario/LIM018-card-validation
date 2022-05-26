8504198b626b4b0c413e73799ead1be9bc91de43
Anais <avalverdea54@gmail.com>
2022-05-26 12:17:14 -0500

    Falta el maskify

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import validator from './validator.js';
+//Obtener el valor
+// let btn_pagar=document.getElementById("pagar");
+// btn_pagar.addEventListener("click",pago);
+// function pago(){
+//     let num=document.getElementById("tarjeta").value;
+//     return num;
+// }     
+console.log(validator.isValid('4083952015263'));
 
-console.log(validator.isValid());
-//Ver ventanas
+
+
+//Boton continuar
 let btn_sgt=document.getElementById("sgt");
 btn_sgt.addEventListener("click", ir);
 function ir(){
@@ -10,12 +19,3 @@ function ir(){
     let vista2=document.getElementById("ventana2");
     vista2.style.display="block";
 }
-//Obtener el valor
-function obtenerNumero(){
-    let num=document.getElementById("tarjeta").value;
-    let miArray= new Array(num);
-    for(let i=num.length-1;i>=0;i--){
-        console.log(numeros[i])
-    }
-}
-console.log(obtenerNumero); 
--- src/validator.js
+++ src/validator.js
@@ -1,10 +1,42 @@
 const validator = {
-  isValid:function(creditCarNumber){
-    console.log("Hola")
-  },
-  maskify:function(creditCarNumber){
-  //..
-  },
+  isValid:function(cCardNumber){
+    let miArray= cCardNumber.split('');//Convertido a Array
+    let inverso=miArray.reverse();//Inverti el array
+    let arrayNum=inverso.map(Number);//Nuevo array convertido a number
+    let unidad=0;
+    let impares=0;
+    let menores=0;
+    let mayores=0;
+    let suma=0;
+    for(let i=0; i<arrayNum.length;i++){
+      if(i%2!=0){ 
+        unidad=arrayNum[i];//[3 2 6 2 7]
+        unidad*=2; //6,4,12,4,14
+        if(unidad>9){ 
+          unidad= 1 + (unidad % 10);//12(3)y 14(5) 
+          mayores+=unidad; //->8
+        }else{
+          menores+=unidad;//6 4 4 ->14
+        }                                                  
+      }else{
+        impares+=arrayNum[i]; // 1 2 6 3 4 ->16
+      }
+    }
+    suma=impares+menores+mayores;
+    console.log(suma);
+    if(suma%10===0){
+      return true;
+    }else{
+      return false 
+    }
+  }, 
+  maskify:function(cCardNumber){
+    // let mascara="#";
+    // let enmascarar= cCardNumber.slice(0,-4).replace(/./g,mascara)+(cCardNumber).slice(-4);
+    // console.log(enmascarar);
+  }
+  
 };
 
+//console.log(validator.isValid());
 export default validator;
