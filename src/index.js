54c3bfdfea23d2c85717beb8654ad72af1cf58b0
Anais <avalverdea54@gmail.com>
2022-05-27 12:11:15 -0500

    falta  mostrar en el input text el ###445

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 import validator from './validator.js';
 //Obtener el valor
-// let btn_pagar=document.getElementById("pagar");
-// btn_pagar.addEventListener("click",pago);
-// function pago(){
-//     let num=document.getElementById("tarjeta").value;
-//     return num;
-// }     
+let btn_pagar=document.getElementById("pagar");
+btn_pagar.addEventListener("click",pago);
+function pago(){
+    let num= document.getElementById("tarjeta").value;
+    let numValidado=validator.isValid(num);
+    //let mask=validator.maskify(num);
+    let mensaje= document.getElementById("msj_Validacion");
+    if(numValidado==true){
+        mensaje.innerHTML='¡¡Tarjeta Valida!! Su pago fue exitoso!!!';
+    }else{
+        mensaje.innerHTML='Tarjeta Invalida';
+    }
+    return mensaje;
+}
+
 //console.log(validator.isValid('4083952015263'));
-validator.isValid('4083952015263');
-validator.isValid('79927398713');
-validator.isValid('1234567890');
-validator.maskify('1');
-validator.maskify('4556364607935616');
-validator.maskify('helloworld');
+//Pasar los test
+// validator.isValid('4083952015263');
+// validator.isValid('79927398713');
+// validator.isValid('1234567890');
+// validator.maskify('1');
+// validator.maskify('4556364607935616');
+// validator.maskify('helloworld');
 
 //Boton continuar
 let btn_sgt=document.getElementById("sgt");
